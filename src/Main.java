17ed2b9010f576775d71657f8e4a9cb1db9bbbba
Agniv0406 <agniv0406@gmail.com>
2026-04-20 08:58:53 +0530

    feature/UC4

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int num,i;
-        String ch;
-        for(i=1;i<=9;i++){
-            System.out.println("Select slot : ");
-            num = sc.nextInt();
-            sc.nextLine();
-            System.out.println("Slot selected : " + num);
-            ch = sc.nextLine();
-            System.out.println("Character in slot selected : " + ch);
+        int num,i,row,column;
+        System.out.println("Enter slot : ");
+        num = sc.nextInt();
+        row = ((num-1)/3);
+        column = (num%3);
+        if(column==0){
+            column = 3;
         }
+        System.out.println("Row : " + row);
+        System.out.println("Column : " + column);
     }
 }
